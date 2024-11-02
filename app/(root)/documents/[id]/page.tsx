529d1139f3577008239734dca61f97f244095904
MrDXTR <manavchillar@gmail.com>
2024-11-03 03:08:34 +0530

    add auth

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,21 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
   return (
     <div>
       <Header>
-        <p className="text-white">Test</p>
+        <div className="flex w-fit justify-center items-center gap-2">
+          <p className="document-title"> Title</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
