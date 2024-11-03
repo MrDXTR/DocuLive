54281f0e19556c191b4cc48e52a50f3f110184ad
MrDXTR <manavchillar@gmail.com>
2024-11-03 05:50:41 +0530

    changes

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,5 +1,11 @@
 import { Liveblocks } from "@liveblocks/node";
 
+if (!process.env.LIVEBLOCKS_SECRET_KEY?.startsWith("sk_")) {
+  throw new Error(
+    "Invalid or missing LIVEBLOCKS_SECRET_KEY environment variable"
+  );
+}
+
 export const liveblocks = new Liveblocks({
-  secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
+  secret: process.env.LIVEBLOCKS_SECRET_KEY,
 });
